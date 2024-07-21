a6e632c31908f2b3e67ef6247f587c8a548d671b
Seksit Disaro <zekzit@gmail.com>
2024-07-21 16:54:11 +0700

    feat: add admin layout

---
+++ src/app/admin/layout.js
@@ -0,0 +1,21 @@
+"use client"
+import { useRouter } from 'next/navigation'
+import React from 'react'
+
+function AdminLayout({ children }) {
+    const router = useRouter();
+    return (
+        <>
+            <div className="w-screen bg-black text-white p-8 flex">
+                <div className=''>Admin Zone</div>
+                <div className='flex-1 flex justify-end'>
+                    <a href="/admin/blogs" className="btn btn-link btn-sm text-white no-underline">Blogs</a>
+                    <a href="/" className="btn btn-link btn-sm text-white no-underline">Logout</a>
+                </div>
+            </div>
+            {children}
+        </>
+    )
+}
+
+export default AdminLayout
