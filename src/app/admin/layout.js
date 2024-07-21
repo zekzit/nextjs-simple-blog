ead9a29ee9242fd0a0b5a50d6f9508fb38f5633c
Seksit Disaro <zekzit@gmail.com>
2024-07-21 16:54:50 +0700

    fix: admin title size

--- src/app/admin/layout.js
+++ src/app/admin/layout.js
@@ -6,8 +6,8 @@ function AdminLayout({ children }) {
     const router = useRouter();
     return (
         <>
-            <div className="w-screen bg-black text-white p-8 flex">
-                <div className=''>Admin Zone</div>
+            <div className="w-screen bg-black text-white p-8 py-4 flex">
+                <div className='mt-1'>Admin Zone</div>
                 <div className='flex-1 flex justify-end'>
                     <a href="/admin/blogs" className="btn btn-link btn-sm text-white no-underline">Blogs</a>
                     <a href="/" className="btn btn-link btn-sm text-white no-underline">Logout</a>
